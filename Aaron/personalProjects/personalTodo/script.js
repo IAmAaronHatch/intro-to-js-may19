43f0f0d59d196cb3810205686bcf9ef2133cf91a
Aaron Hatch <aaron@aarons-mbp.attlocal.net>
2019-06-01 13:25:30 -0700

    strikethrough when complete works

--- Aaron/personalProjects/personalTodo/script.js
+++ Aaron/personalProjects/personalTodo/script.js
@@ -10,21 +10,41 @@ const create = (type, parent, text) => {
 }   
 
 const input = create(`input`, body);
+input.placeholder = `Enter Tasks Here`;
 const submitBtn = create(`button`, body, `Submit`);
 const list = create(`ul`, body);
+list.classList.add(`main-ul`);
+
+const strikeThrough = (el, strike) => {
+    // strike = !strike;
+    
+    if (!strike) {
+        el.classList.remove(`finished`);                
+    } else if (strike){
+        el.classList.add(`finished`);        
+    }
+}  
 
 const newTodo = () => {
     let value = input.value;
 
     if (value) {
         let listItem = create('li', list, value);
+        listItem.classList.add('item')
         
         input.value = ``;
 
+        let strike= false;
+
         listItem.addEventListener(`click`, function(){
-            listItem.classList.add(`finished`);
+            strike = !strike;            
+            strikeThrough(listItem, strike);
+            
         })
     };
 }
+
+ 
+
 
 submitBtn.addEventListener(`click`, newTodo);
