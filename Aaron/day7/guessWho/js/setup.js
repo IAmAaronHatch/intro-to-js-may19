12b93dde1285fb092398a4c03ec2ec30be241b8d
Aaron Hatch <aaron@aarons-mbp.attlocal.net>
2019-06-12 14:23:22 -0700

    change the characters based on traits

--- Aaron/day7/guessWho/js/setup.js
+++ Aaron/day7/guessWho/js/setup.js
@@ -10,6 +10,18 @@ const people = [
     ['claire', 'girl', 'hat'], ['david'], ['eric', 'hat'], ['frans'], ['george', 'hat'], ['herman'], ['joe'], ['maria', 'girl', 'hat'], ['max'], ['paul'], ['peter'], ['phillip'], ['richard'], ['robert'], ['sam'], ['susan', 'girl'], ['tom']
 ];
 
+const keywords = [];
+
+for(let i = 0; i < 23; i++) {
+    for(let j = 0; j < people[i].length; j++) {
+        if (!keywords.includes(people[i][j])) {
+            keywords.push(people[i][j]);
+        }
+    
+    }
+}
+console.log(keywords, 'keywords')
+let suspect = null;
 
 function setup() {
     const container = document.createElement(`div`);
@@ -27,7 +39,35 @@ function setup() {
         container.appendChild(card);
 
         card.innerText = people[i][0];
+        card.classList.add(people[i])
     }
+
+    const textBox = document.createElement('textarea');
+    textBox.addEventListener('input', function(){
+        let trait = null;
+        if(textBox.value && textBox.value.slice(-1)=== '?'){
+            const value = textBox.value.slice(0, textBox.value.length - 1);
+            textBox.value = '';
+            value.split(' ').forEach(word => {
+                if(keywords.includes(word)) {
+                    trait = word
+                }
+            })
+
+            if(suspect.classList.contains(trait)) {
+                document.querySelectorAll('.char').forEach(char => {
+                    if(char.classList.contains(trait)) {
+                        char.style.display = 'none';
+                    }
+                })
+            }
+        }
+    })
+    document.body.appendChild(textBox);
+
+    const randomNumber = Math.floor(Math.random() * (columns * rows));
+    suspect = document.querySelectorAll('.char')[randomNumber];
+    console.log('sus', suspect)
 };
 
 function createCard(x, y) {
